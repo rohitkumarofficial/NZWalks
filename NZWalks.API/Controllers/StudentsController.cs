f91f905c3e09a74ddbfd62c96e98bfdedca9aae2
Rohit Kumar <ROHITKUMARJAJORIYA@GMAIL.COM>
2024-02-21 21:28:17 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+  
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        //https://localhost:port/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            return Ok(new string[] { "Rohit" });
+        }
+    }
+}
